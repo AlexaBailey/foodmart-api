ef07b34d52db508582727abf71b8a00d2d2e0f97
AlexaBailey <alexaserko@gmail.com>
2024-03-26 20:06:56 +0300

    Add backend

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: 'https://foodmart-project-production-91c8.up.railway.app', // use your actual domain name (or localhost), using * is not recommended
+  origin: 'https://foodmart-project-production-91c8.up.railway.app/', // use your actual domain name (or localhost), using * is not recommended
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
   credentials: true
