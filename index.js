b8473764dbe3802384e9177171c3b8dc728744a1
AlexaBailey <alexaserko@gmail.com>
2024-03-26 20:12:17 +0300

    Add backend

--- index.js
+++ index.js
@@ -46,7 +46,11 @@ app.use(cors({
   credentials: true
 }))
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type, Accept");
+  next();
+});
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
