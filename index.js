098b2f7b1403a3b7703375cf29a30d075260f25f
AlexaBailey <alexaserko@gmail.com>
2024-03-26 16:40:42 +0300

    Add backend

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ const upload = multer({
 });
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://foodmart-project-production-91c8.up.railway.app/'); // Replace with your frontend URL
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
