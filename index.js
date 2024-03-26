d3d6a4ded4f811952f2dc0b287d77f1e6e1d6bf9
AlexaBailey <alexaserko@gmail.com>
2024-03-26 15:31:53 +0300

    Add backend

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const db = mysql.createConnection({
   host:process.env["DB_HOST "],
   password: process.env["DB_PASSWORD "],
   database:process.env["DB_NAME "],
-  port:28356,
+  port:33060,
   multipleStatements: true,
 }); 
 
